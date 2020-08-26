e948fb4f94667c8f66e6c9fc20ff7ad5795778f6
kevin_f_liu <1663531467@qq.com>
2020-08-26 13:53:50 +0800

    [pocker hands] [kevin && david] feature: pass should_return_Player_2_wins_when_compare_given_palyer1_2H3D5S9CKD_and_player2_3H3D5S9CKD

--- src/main/java/com/example/PockerHands.java
+++ src/main/java/com/example/PockerHands.java
@@ -5,10 +5,20 @@ import static java.util.Collections.max;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
 
 public class PockerHands {
 
   public String compare(String[] player_1, String[] player_2) {
+    HashSet<Character> player_1_set=transToSet(player_1);
+    HashSet<Character> player_2_set=transToSet(player_2);
+    if (player_1_set.size()<player_2_set.size()){
+      return "Player 1 wins";
+    }
+    if (player_1_set.size()>player_2_set.size()) {
+      return "Player 2 wins";
+    }
     if(isPlayer2Win(player_1,player_2)){
       return "Player 2 wins";
     }
@@ -40,6 +50,13 @@ public class PockerHands {
     return player_numbers;
   }
 
+  private HashSet<Character> transToSet(String[] player){
+    HashSet<Character> player_set=new HashSet<>();
+    for (String s : player) {
+      player_set.add(s.charAt(0));
+    }
+    return player_set;
+  }
 
 
 }
